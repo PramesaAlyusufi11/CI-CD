c37a709a6960951fb5a3bf084bdd8712100a9a88
Alpramm <alpram.alyusuf@gmail.com>
2026-01-21 14:53:50 +0700

    Enhance index.php with HTML and greeting updates
    Added HTML structure and updated greeting messages.

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
-echo "Hallo Alpram, Selmat belajar CI/CD!!
+echo "Praktek CI/CD menggunakan Github & Railway";
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <script src="https://tailwindcss.com"></script>
+    <title>CI/CD</title>
+</head>
+<body>
+    <h1 class="text-lg font-red items-center">Hallo All</h1>
+</body>
+</html>
